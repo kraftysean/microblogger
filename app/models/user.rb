4300d342ff27267aea7fe118298f94b772825890
kraftysean <kraftysm@gmail.com>
2017-08-17 16:13:29 +0100

    Add activation attributes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  # before_save { self.email = email.downcase }
-  before_save { email.downcase! }  # same as version commented out above
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX},
                     uniqueness: { case_sensitive: false }
@@ -39,4 +39,17 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  # Converts email to all lower-case
+  def downcase_email
+    email.downcase!  # same as 'self.email = email.downcase'
+  end
+
+  private
+  
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
